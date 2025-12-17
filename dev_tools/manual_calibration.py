ad988149b4efa3847d1058c5fce7e32b111c10e1
Jessica Rehak <rehak@lbl.gov>
2025-12-17 14:37:32 -0800

    updated manual calibration to use services

--- dev_tools/manual_calibration.py
+++ dev_tools/manual_calibration.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
+from ops.auxiliary.drivers.measurement import ValueMeasurement
 from ops.auxiliary.drivers.voltage_ratio_bridge import VoltageRatioBridge
+from ops.auxiliary.devices.load_cell import LoadCell
+from ops.auxiliary.services.load_cell import LoadCellAquisitionService
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -8,12 +11,54 @@ logging.basicConfig(
 )
 _log = logging.getLogger("ops")
 
+Channel = VoltageRatioBridge.Channel
+
+
+async def broadcast_data(queue: asyncio.Queue):
+    while True:
+        data: ValueMeasurement = await queue.get()
+        print(f"{data.source} weight:{data.value:>30.0f}")
+        queue.task_done()
+
 
 async def calibrate():
-    channels = [VoltageRatioBridge.Channel.CHANNEL_0, VoltageRatioBridge.Channel.CHANNEL_3]
-
+    channels = [Channel.CHANNEL_0, Channel.CHANNEL_3]
     bridge = VoltageRatioBridge(channels_to_connect=channels)
-    await bridge.connect()
+
+    load_cell_0 = LoadCell(bridge, Channel.CHANNEL_0, id="Load cell, channel 0")
+    await load_cell_0.connect()
+    load_cell_3 = LoadCell(bridge, Channel.CHANNEL_3, id="Load cell, channel 3")
+    await load_cell_3.connect()
+
+    await load_cell_0.calibrate()
+    await load_cell_3.calibrate()
+
+    load_cell_0_service = LoadCellAquisitionService(load_cell_0, 1.0)
+    load_cell_3_service = LoadCellAquisitionService(load_cell_3, 1.0)
+
+    try:
+        _log.info("Starting services...")
+        await load_cell_0_service.start()
+        await load_cell_3_service.start()
+        _log.info("All services running.")
+
+        tasks = [
+            broadcast_data(load_cell_0_service._data_queue),
+            broadcast_data(load_cell_3_service._data_queue),
+        ]
+        _log.info("Application is running. Press Ctrl+C to exit")
+        await asyncio.gather(*tasks)
+
+    except (KeyboardInterrupt, asyncio.CancelledError):
+        _log.info("Shutdown signal received...")
+    except Exception:
+        _log.exception("An unknown exception occurred, shutting down...")
+    finally:
+        _log.info("Cleaning up resources...")
+        await asyncio.gather(
+            load_cell_0_service.stop(), load_cell_3_service.stop(), return_exceptions=True
+        )
+        _log.info("Cleanup complete. Exiting.")
 
 
 if __name__ == "__main__":
